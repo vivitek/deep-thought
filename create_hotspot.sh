c6f738e5af9a712c8e206dc9b0beebacdff6ccea
Ubuntu <ubuntu@ubuntu.localdomain>
2021-03-08 21:57:15 +0000

    [add] added router and init script for hotspot creation

---
+++ create_hotspot.sh
@@ -0,0 +1,3 @@
+sudo nmcli con add type wifi ifname wlan0 con-name 'vivi hotspot' autoconnect yes ssid 'viviHotspot'
+sudo nmcli connection   modify "vivi hotspot"   802-11-wireless.mode ap   802-11-wireless.band bg   ipv4.method shared
+sudo nmcli con up "vivi hotspot"
