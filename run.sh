5c69a1774e790bad5f3d697829e8b2026f20262f
Ubuntu <ubuntu@ubuntu.localdomain>
2021-03-09 10:57:07 +0000

    [add] init_db scripts, but db does not work

--- run.sh
+++ run.sh
@@ -1,4 +1,5 @@
 sh create_hotspot.sh
 sh install_nft.sh
 sh setup_virtualenv.sh
+sh init_db.sh
 sh run-firewall.sh
--- setup_virtualenv.sh
+++ setup_virtualenv.sh
@@ -1,7 +1,7 @@
 cd firewall
 sudo pip install -r requirements.txt
 cd ..
-export DATABASE_URL=postgresql://rbouvard:postgrespwd@localhost:5432/firewallDB
+export DATABASE_URL=postgresql://rbouvard:postgrespwd@localhost:5432/firewalldb
 sudo -E python3 ./firewall/manage.py db init
 sudo -E python3 ./firewall/manage.py db migrate
 sudo -E python3 ./firewall/manage.py db upgrade
